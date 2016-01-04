5193c5ad11cffc8f24a3d0811210084a72baa6e1
Caesar <caesarjuming@gmail.com>
2016-01-04 13:09:18 +0800

    Dijkstra

--- src/ShortestPaths/Dijkstra.java
+++ src/ShortestPaths/Dijkstra.java
@@ -5,13 +5,10 @@ package ShortestPaths;
  */
 public class Dijkstra {
 
-
-
     public void GetDijkstraPath(MGraph g,int [] path,int []dist,int Who){
         boolean []visited=new boolean[MGraph.N];
         //initialize
-        int i;
-        for (i=0;i<MGraph.N;i++){
+        for (int i=0;i<MGraph.N;i++){
             if(g.getMatrix(Who,i)>0&&i!=Who){
                 dist[i]=g.getMatrix(Who,i);
                 path[i]=Who;
@@ -25,15 +22,29 @@ public class Dijkstra {
             dist[Who]=0;
 
         }
+        visited[Who]=true;
+        //find the min value
+        int MIN=Integer.MAX_VALUE;
+        int m=-1;
+        for(int j=1;j<MGraph.N;j++){
 
-
+            m=-1;
+            for(int k=0;k<MGraph.N;k++){
+                if(visited[k]==false&&dist[k]<MIN){
+                  m=k;
+                  MIN=dist[k];
+                }
+            }
+        }
+        // union find
+        for(int l=1;l<MGraph.N;l++){
+            if(visited[l]==false&&g.getMatrix(m,l)+MIN<dist[l]){
+                dist[l]=g.getMatrix(m,l)+MIN;
+                path[l]=m;
+            }
+        }
 
     }
-
-
-
-
-
 }
 
 
